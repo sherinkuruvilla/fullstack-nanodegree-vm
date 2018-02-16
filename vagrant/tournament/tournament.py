5f58254b5c23cd38a40b24adbf80407501ac2ff5
sherinkuruvilla <10779078+sherinkuruvilla@users.noreply.github.com>
2018-02-15 21:50:54 -0500

    Add files via upload

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -8,36 +8,57 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    return psycopg2.connect("dbname=tournament user=postgres password=sherin")
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("delete from matches;")
+    db.commit()
+    db.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    c = db.cursor()
+    c.execute("delete from players;")
+    db.commit()
+    db.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    c = db.cursor()
+    c.execute("select count(*) from players;")
+    playercount = c.fetchall()[0][0]
+    db.close()
+    return playercount
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    db = connect()
+    c = db.cursor()
+    c.execute("insert into players values(%s)", (name,))
+    db.commit()
+    db.close()
 
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
+    The first entry in the list should be the player in first place, or aplayer
     tied for first place if there is currently a tie.
 
     Returns:
@@ -46,7 +67,21 @@ def playerStandings():
         name: the player's full name (as registered)
         wins: the number of matches the player has won
         matches: the number of matches the player has played
+
+    All players from table show their wins, losses and opponent score
+    Player standing is decided by ordering by wins and then by opponent's
+    average wins.
+    This ensures that those with a tie will break tie when they have beaten a
+    stronger opponent than the other on average
     """
+    db = connect()
+    c = db.cursor()
+    c.execute("""
+            select id, name, wins, matches from
+            player_standings order by wins desc, opp_wins desc;""")
+    player_standings = c.fetchall()
+    db.close()
+    return player_standings
 
 
 def reportMatch(winner, loser):
@@ -56,16 +91,21 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    db = connect()
+    c = db.cursor()
+    c.execute("insert into matches values(%s, %s)", (winner, loser,))
+    db.commit()
+    db.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +113,20 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
+    """
+    The base view is player_standings sorted by wins desc & tiebreaker scores
+    The partner view is essentially the same as base view but offset by 1
+    This is to ensure that if id1, id2, id3 and id4 are as per player_standing,
+    the partner view will show id2, id3 and id4 with matching row number
+    as first view.
+    once joined on matching row numbers, and skipping alternate rows
+    we will ensure we will get id1 and id2 paired etc.
+    """
+    db = connect()
+    c = db.cursor()
+    c.execute("""
+            select id, name, partner_id, partner_name from
+            swiss_ranking;""")
+    swiss_ranking = c.fetchall()
+    db.close()
+    return swiss_ranking
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,108 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+drop database tournament;
+
+create database tournament;
+
+\c tournament;
+
+drop table if exists players cascade;
+
+create table players(
+  name text,
+  id serial PRIMARY KEY
+);
+
+drop table if exists matches cascade;
+
+create table matches (
+  winner_id integer REFERENCES players(id),
+  loser_id integer REFERENCES players(id),
+   PRIMARY KEY (winner_id, loser_id)
+);
+
+
+-- INITIAL BASE VIEW created to keep track of player, number of wins and losses.
+create or replace view score
+as
+  select
+  id,
+  name,
+  coalesce(wins,0) wins,
+  coalesce(losses,0) losses
+from
+  players
+  left join (select winner_id, count(*) wins from matches group by winner_id) win
+    on players.id = win.winner_id
+  left join  (select loser_id, count(*) losses from matches group by loser_id) loss
+    on players.id = loss.loser_id;
+
+
+-- VIEW built on top of score VIEW.
+-- Using score, calculate opponent score to break a tie.
+create or replace view opp_score
+as
+  select
+  winner_id as id,
+  round(avg(wins),2) opp_wins
+from
+  matches
+  left join score
+    on matches.loser_id=score.id
+group by
+  winner_id;
+
+-- VIEW to provide player_standings
+-- All players from table show their wins, losses and opponent score
+-- Player standing is decided by ordering by wins and then by opponent's average wins.
+-- This ensures that those with a tie will break tie when they have beaten a
+-- stronger opponent than the other on average
+create or replace view player_standings
+as
+  select
+  players.id,
+  players.name,
+  wins+losses as matches,
+  wins,
+  opp_wins
+from
+  players
+  left join score
+    on players.id = score.id
+  left join opp_score
+    on players.id = opp_score.id
+order by
+  wins desc, opp_wins desc;
+
+--VIEW to determine pairing based on top scores
+--The base view is player_standings sorted by wins desc and tie breaker scores
+--The partner view is essentially the same as base view but offset by 1
+--This is to ensure that if id1, id2, id3 and id4 are as per player_standing,
+--the partner view will show id2, id3 and id4 with matching row number as first view.
+--once joined on matching row numbers, and skipping alternate rows
+-- we will ensure we will get id1 and id2 paired etc.
+create view swiss_ranking
+as
+  select
+  id,
+  name,
+  partner_id,
+  partner_name
+from
+  (select id, name, row_number() over (order by wins desc, opp_wins desc) rank
+    from player_standings) player
+  left join (select id  partner_id, name partner_name, row_number() over (order by wins desc, opp_wins desc)-1 rank
+              from player_standings  offset 1) partner
+                on player.rank=partner.rank
+where player.rank % 2=1;
+
+
+
+
+
+
+
+
+
 
